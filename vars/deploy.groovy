cb197ff669d51de23c2ede82295b8fd788d206f3
SumaVarshitha <sumavarshitha.kamatam997@gmail.com>
2020-06-30 10:57:04 +0530

    Create deploy.groovy

---
+++ vars/deploy.groovy
@@ -0,0 +1,7 @@
+def call(String msg = 'deploy') {
+echo "${msg}"
+   sh 'curl http://18.216.136.124:8080//manager/text/undeploy?path=/bunny -u admin:admin'
+	 sh 'curl -v -u admin:admin -T target/java-webapp.war http://18.216.136.124:8080//manager/text/deploy?path=/bunny'
+		
+		
+}
