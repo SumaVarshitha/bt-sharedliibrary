8211b6df0aaedc8a1a513db6a8fb91384e629ff9
SumaVarshitha <sumavarshitha.kamatam997@gmail.com>
2020-06-30 11:25:50 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,7 +1,8 @@
-def call() {
 
+def call(String msg = 'deploy to tomcat') {
+echo "${msg}"
    sh 'curl http://18.216.136.124:8080//manager/text/undeploy?path=/bunny -u admin:admin'
-	 sh 'curl -v -u admin:admin -T target/java-webapp.war http://18.216.136.124:8080//manager/text/deploy?path=/bunny'
+   sh 'curl -v -u admin:admin -T target/java-webapp.war http://18.216.136.124:8080//manager/text/deploy?path=/bunny'
 		
 		
 }
