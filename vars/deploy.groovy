8c6c40200f793c9d4451eaa7e9678451337609d4
SumaVarshitha <sumavarshitha.kamatam997@gmail.com>
2020-06-30 11:38:53 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,6 +1,6 @@
 
-def call(String msg ='deploy') {
-echo "${msg}"
+def call() {
+
    sh 'curl http://18.216.136.124:8080//manager/text/undeploy?path=/bunny -u admin:admin'
    sh 'curl -v -u admin:admin -T target/java-webapp.war http://18.216.136.124:8080//manager/text/deploy?path=/bunny'
 		
