cfc2df24521e442df18ed2a197b44104788f742f
SumaVarshitha <sumavarshitha.kamatam997@gmail.com>
2020-07-08 12:56:56 +0530

    Update sonarqube.groovy

--- vars/sonarqube.groovy
+++ vars/sonarqube.groovy
@@ -4,7 +4,8 @@ environment{
       sonarscanner = tool 'sonars'
           }
     withSonarQubeEnv('sonar'){
-      sh '${sonarscanner}/bin/sonar-scanner -Dproject.settings=./sonar-project.properties'
+      sh '${sonarscanner}/bin/sonar-scanner
+	    //-Dproject.settings=./sonar-project.properties'
 		       //sh "${scannerHome}/bin/sonar-scanner"
               // sh 'mvn sonar:sonar' 
     }
