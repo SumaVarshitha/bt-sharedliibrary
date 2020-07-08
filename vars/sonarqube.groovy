3479b53fee8b10be961b3f4f7d307496598925d0
SumaVarshitha <sumavarshitha.kamatam997@gmail.com>
2020-07-08 13:00:47 +0530

    Update sonarqube.groovy

--- vars/sonarqube.groovy
+++ vars/sonarqube.groovy
@@ -4,8 +4,7 @@ environment{
       sonarscanner = tool 'sonars'
           }
     withSonarQubeEnv('sonar'){
-      sh '${sonarscanner}/bin/sonar-scanner
-	    //-Dproject.settings=./sonar-project.properties'
+      sh '${sonarscanner}/bin/sonar-scanner -Dproject.settings=/var/lib/jenkins/workspace/sonar-project.properties'
 		       //sh "${scannerHome}/bin/sonar-scanner"
               // sh 'mvn sonar:sonar' 
     }
