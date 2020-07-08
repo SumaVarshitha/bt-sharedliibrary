98776a87bfcf750dc016a6aeb1698204bd5425e3
SumaVarshitha <sumavarshitha.kamatam997@gmail.com>
2020-07-08 12:36:59 +0530

    Create sonarqube.groovy

---
+++ vars/sonarqube.groovy
@@ -0,0 +1,12 @@
+def call()
+{
+environment{
+               sonarscanner = tool 'sonars'
+                   }
+                    withSonarQubeEnv('sonar'){
+                    sh '${sonarscanner}/bin/sonar-scanner -Dproject.settings=./sonar-project.properties'
+		       //sh "${scannerHome}/bin/sonar-scanner"
+              // sh 'mvn sonar:sonar' 
+	       }
+
+}
