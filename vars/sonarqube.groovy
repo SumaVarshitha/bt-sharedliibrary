2553e0034e7c7a6582f11c771c36e7888df6913e
SumaVarshitha <sumavarshitha.kamatam997@gmail.com>
2020-07-09 15:16:24 +0530

    Update sonarqube.groovy

--- vars/sonarqube.groovy
+++ vars/sonarqube.groovy
@@ -4,7 +4,7 @@ environment{
       sonarscanner = tool 'sonars'
           }
     withSonarQubeEnv('sonar'){
-      sh '${sonarscanner}/var/lib/jenkins/tools/hudson.plugins.sonar.SonarRunnerInstallation/SonarQube_Scanner/bin/sonar-scanner -Dproject.settings=./sonar-project.properties'
+      sh '/var/lib/jenkins/tools/hudson.plugins.sonar.SonarRunnerInstallation/${sonarscanner}/bin/sonar-scanner -Dproject.settings=./sonar-project.properties'
 		       //sh "${scannerHome}/bin/sonar-scanner"
               // sh 'mvn sonar:sonar' 
     }
