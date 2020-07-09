8f879e3e5dbddb72ad36d959de4c4707f2e339fd
SumaVarshitha <sumavarshitha.kamatam997@gmail.com>
2020-07-09 12:13:26 +0530

    Delete dep.groovy

--- vars/dep.groovy
+++
@@ -1,8 +0,0 @@
-
-def call() {
-
-   sh 'curl http://18.216.136.124:8080//manager/text/undeploy?path=/bunny -u admin:admin'
-   sh 'curl -v -u admin:admin -T target/java-webapp.war http://18.216.136.124:8080//manager/text/deploy?path=/bunny'
-		
-		
-}
