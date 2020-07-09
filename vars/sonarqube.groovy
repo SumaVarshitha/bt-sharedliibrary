a1eb3ab50a762272c137cd1e7399f13a84145d50
SumaVarshitha <sumavarshitha.kamatam997@gmail.com>
2020-07-09 19:32:05 +0530

    Update sonarqube.groovy

--- vars/sonarqube.groovy
+++ vars/sonarqube.groovy
@@ -7,7 +7,8 @@ environment{
           }
     withSonarQubeEnv('sonar'){
       //sh '/var/lib/jenkins/tools/hudson.plugins.sonar.SonarRunnerInstallation/${sonarscanner}/bin/sonar-scanner -Dproject.settings=./sonar-project.properties'
-	    sh """${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=SumaVarshitha_assessmentdocker -Dsonar.organization=sumavarshitha -Dsonar.projectName=assessmentdocker -Dsonar.projectVersion=1.0 -Dsonar.language=java -Dsonar.sources=src -Dsonar.java.binaries=. -Dsonar.host.url=https://sonarcloud.io/ -Dsonar.login=23c3c4a2d8cdc3f94ecc357df8d681a601cb48f6 -Dsonar.java.coveragePlugin=jacoco -Dsonar.sourceEncoding=UTF-8 -Dsonar.coverage.jacoco.xmlReportPaths = target/site/jacoco/*.xml=true """
+	    //sh """${scannerHome}/bin/sonar-scanner 
+	    sh """ mvn sonar:sonar -Dsonar.projectKey=SumaVarshitha_assessmentdocker -Dsonar.organization=sumavarshitha -Dsonar.projectName=assessmentdocker -Dsonar.projectVersion=1.0 -Dsonar.language=java -Dsonar.sources=src -Dsonar.java.binaries=. -Dsonar.host.url=https://sonarcloud.io/ -Dsonar.login=23c3c4a2d8cdc3f94ecc357df8d681a601cb48f6 -Dsonar.java.coveragePlugin=jacoco -Dsonar.sourceEncoding=UTF-8 -Dsonar.coverage.jacoco.xmlReportPaths = target/site/jacoco/*.xml=true """
     }
 
 }
