f74c4f63be81826a8f67fef1034c4f9447909864
SumaVarshitha <sumavarshitha.kamatam997@gmail.com>
2020-07-10 14:50:59 +0530

    Create pipeline.groovy

---
+++ vars/pipeline.groovy
@@ -0,0 +1,42 @@
+pipeline {
+	
+	 
+    agent any
+  
+    stages {
+	    stage('clonestage'){
+		    steps{
+		  
+		    clonerepo()
+		    }}
+		 
+        stage('build') {
+            steps {
+			  
+       
+		    dockerbuild()
+                }
+			
+            
+	    }
+        
+        stage('SonarQube Analysis'){
+		
+		 
+          steps{
+         
+		    sonarqube()
+	       
+            }
+       }
+
+    
+        
+      stage("Quality Gate") {
+            steps {
+		    qualitygate()
+             
+           }
+        }
+    }
+}
