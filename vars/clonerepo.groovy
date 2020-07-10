337cc715e5a70e921787d2017abffed96ba96bff
SumaVarshitha <sumavarshitha.kamatam997@gmail.com>
2020-07-10 07:37:13 +0530

    Update clonerepo.groovy

--- vars/clonerepo.groovy
+++ vars/clonerepo.groovy
@@ -1,6 +1,6 @@
 def call()
 {
-                sh 'rm -rf assessmentdocker' 
-	        sh 'git clone https://github.com/SumaVarshitha/assessmentdocker.git'
+                sh 'rm -rf java-maven-junit-helloworld' 
+	        sh 'git clone https://github.com/SumaVarshitha/java-maven-junit-helloworld.git'
           
   }
