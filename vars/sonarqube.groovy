6c100a57f9b27d3f8e36bcc1587368c62e9d7b3b
SumaVarshitha <sumavarshitha.kamatam997@gmail.com>
2020-07-10 16:49:05 +0530

    Update sonarqube.groovy

--- vars/sonarqube.groovy
+++ vars/sonarqube.groovy
@@ -3,8 +3,8 @@ def call()
   {
     
     def scannerHome = tool 'sonars'
-    withSonarQubeEnv(credentialsId: 'sonarqubenew', installationName: 'sonar'){
-      sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=SumaVarshitha_assessmentdocker -Dsonar.organization=sumavarshitha -Dsonar.projectName=assessmentdocker -Dsonar.projectVersion=1.0  -Dsonar.language=java -Dsonar.sources=. -Dsonar.java.binaries=. -Dsonar.host.url=https://sonarcloud.io/ -Dsonar.java.coveragePlugin=jacoco -Dsonar.sourceEncoding=UTF-8 -Dsonar.coverage.jacoco.xmlReportPaths=target/site/jacoco/jacoco.xml -Dsonar.exclusions=**/*.html,**/*.css,**/*.js,**/*.xml,src/test/java/com/puppet/sample/PolyglotTest.java"
+    withSonarQubeEnv('sonar'){
+      sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=SumaVarshitha_assessmentdocker -Dsonar.organization=sumavarshitha -Dsonar.projectName=assessmentdocker -Dsonar.projectVersion=1.0 -Dsonar.login=23c3c4a2d8cdc3f94ecc357df8d681a601cb48f6  -Dsonar.language=java -Dsonar.sources=. -Dsonar.java.binaries=. -Dsonar.host.url=https://sonarcloud.io/ -Dsonar.java.coveragePlugin=jacoco -Dsonar.sourceEncoding=UTF-8 -Dsonar.coverage.jacoco.xmlReportPaths=target/site/jacoco/jacoco.xml -Dsonar.exclusions=**/*.html,**/*.css,**/*.js,**/*.xml,src/test/java/com/puppet/sample/PolyglotTest.java"
 		    
 	       }
             }
