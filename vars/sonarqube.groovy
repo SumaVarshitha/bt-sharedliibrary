7668e6000eece7116dd4568b1b9c6395d70ec42d
SumaVarshitha <sumavarshitha.kamatam997@gmail.com>
2020-07-10 15:56:33 +0530

    Update sonarqube.groovy

--- vars/sonarqube.groovy
+++ vars/sonarqube.groovy
@@ -3,7 +3,7 @@ def call()
   {
       def token = CredentialsID 'sonarqubenew'
     def scannerHome = tool 'sonars'
-    withSonarQubeEnv('sonar'){
+    withSonarQubeEnv(credentialsId: 'sonarqubenew', installationName: 'sonar'){
       sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=SumaVarshitha_assessmentdocker -Dsonar.organization=sumavarshitha -Dsonar.projectName=assessmentdocker -Dsonar.projectVersion=1.0  -Dsonar.language=java -Dsonar.sources=. -Dsonar.java.binaries=. -Dsonar.host.url=https://sonarcloud.io/ -Dsonar.login=${token} -Dsonar.java.coveragePlugin=jacoco -Dsonar.sourceEncoding=UTF-8 -Dsonar.coverage.jacoco.xmlReportPaths=target/site/jacoco/jacoco.xml -Dsonar.exclusions=**/*.html,**/*.css,**/*.js,**/*.xml,src/test/java/com/puppet/sample/PolyglotTest.java"
 		    
 	       }
