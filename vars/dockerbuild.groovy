930f44af2d8077527f453d2c2213d802b2e4d87e
SumaVarshitha <sumavarshitha.kamatam997@gmail.com>
2020-07-16 11:11:34 +0530

    Update dockerbuild.groovy

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,6 +1,10 @@
 def call(dockerimage,mavenBuild) 
 {
+        echo dockerimage
+        echo mavenBuild
+        def dimage = dockerimage
+        def mBuild = maveenBuild
         docker.image("${dockerimage}").inside(){
-          sh "${mavenBuild}"
+          sh "${mBuild}"
         }}
             
