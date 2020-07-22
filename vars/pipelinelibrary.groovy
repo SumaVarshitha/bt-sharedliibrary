5db030a7387028eecbad9e5b23fb2d5c3a712e36
SumaVarshitha <sumavarshitha.kamatam997@gmail.com>
2020-07-22 16:43:50 +0530

    Update pipelinelibrary.groovy

--- vars/pipelinelibrary.groovy
+++ vars/pipelinelibrary.groovy
@@ -30,7 +30,7 @@ def sonarsourceEncoding = config.sonarsourceEncoding ?: ''
                 stage('Code Checkout'){
                     steps{
                         clonerepo{
-                            url = "${urllink}
+                            url = "${urllink}"
                             
                         }
                     }
