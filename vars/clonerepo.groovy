ebee5c1db4793335e191f73654b6bd970a77bc75
SumaVarshitha <sumavarshitha.kamatam997@gmail.com>
2020-07-22 16:24:51 +0530

    Update clonerepo.groovy

--- vars/clonerepo.groovy
+++ vars/clonerepo.groovy
@@ -1,8 +1,18 @@
-def call(urllink) {
+/*def call(urllink) {
 	echo urllink
 	def url = urllink
      
 	sh 'rm -rf assessmentdocker' 
 	sh "git clone ${url}"
           
-  }
+  }*/
+def call(body) {    
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+def urllink = config.urllink ?: ''
+	sh 'rm -rf assessmentdocker' 
+	sh "git clone ${urllink}"
+}
+	
