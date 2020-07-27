e0fddbefa8b8349ea1219637bdc012251a592dce
SumaVarshitha <sumavarshitha.kamatam997@gmail.com>
2020-07-27 14:42:24 +0530

    Update dockerbuild.groovy

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -12,7 +12,7 @@ def call(dockerimage,mavenBuild)
                             .getItemByFullName(env.JOB_NAME)
                               .getBuildByNumber(
                               Integer.parseInt(env.BUILD_NUMBER))
-                              .getStageByName(env.STAGE_NAME)
+                              .getStageByName(STAGE_NAME)
                               .logFile.text
                         // copy the log in the job's own workspace
                       writeFile file: "buildlog.txt", text: logContent
