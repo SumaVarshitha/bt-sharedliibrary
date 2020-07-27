31b791ab8967c7a0db9b2db9cecda11dff73a439
SumaVarshitha <sumavarshitha.kamatam997@gmail.com>
2020-07-27 16:09:46 +0530

    Update dockerbuild.groovy

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -7,12 +7,14 @@ def call(dockerimage,mavenBuild)
         def mBuild = mavenBuild
         
         docker.image("${dockerimage}").inside(){
-          sh "${mBuild} > output.txt"
+          //sh "${mBuild} > output.txt"
+                 sh "${mBuild} > output"
                 
         }
               
-        def buildlog = readFile "${WORKSPACE}/output.txt"
-       echo buildlog
+        //def buildlog = readFile "${WORKSPACE}/output.txt"
+      // echo buildlog
+        echo output
                 
         
       /*  def logContent = Jenkins.getInstance()
