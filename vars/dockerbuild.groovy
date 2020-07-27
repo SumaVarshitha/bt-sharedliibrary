b373a1e43c80fc4d557d0859eabea45493e48c5a
SumaVarshitha <sumavarshitha.kamatam997@gmail.com>
2020-07-27 14:32:27 +0530

    Update dockerbuild.groovy

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,5 +1,4 @@
-//import groovy.json.*
-//import groovy.json.JsonSlurper
+
 def call(dockerimage,mavenBuild) 
 {
         echo dockerimage
@@ -15,12 +14,9 @@ def call(dockerimage,mavenBuild)
                               Integer.parseInt(env.BUILD_NUMBER))
                               .logFile.text
                         // copy the log in the job's own workspace
-                      writeFile file: "buildlog.txt", text: logContent
-        def version = readFile "${WORKSPACE}/buildlog.txt"
-        echo version
-        /* def jsonSlurper = new JsonSlurper()
- def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/result.json"),"UTF-8"))
-def resultJson = jsonSlurper.parse(reader)
-        echo resultJson*/
+                      writeFile file: "buildlog.json", text: logContent
+      //  def version = readFile "${WORKSPACE}/buildlog.json"
+       // echo version
+        
 }
             
