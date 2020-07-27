52e3c85ed699e6b014ba0ec14dd7814dd5c3829c
SumaVarshitha <sumavarshitha.kamatam997@gmail.com>
2020-07-27 13:53:22 +0530

    Update dockerbuild.groovy

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -9,6 +9,15 @@ def call(dockerimage,mavenBuild)
         docker.image("${dockerimage}").inside(){
           sh "${mBuild}"
         }
+        def logContent = Jenkins.getInstance()
+                            .getItemByFullName(env.JOB_NAME)
+                              .getBuildByNumber(
+                              Integer.parseInt(env.BUILD_NUMBER))
+                              .logFile.text
+                        // copy the log in the job's own workspace
+                      writeFile file: "buildlog.txt", text: logContent
+        def version = readFile "${WORKSPACE}/buildlog.txt"
+        echo version
         /* def jsonSlurper = new JsonSlurper()
  def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/result.json"),"UTF-8"))
 def resultJson = jsonSlurper.parse(reader)
