0a635dda6a76fc0ab8bf7b090fdb1f19d069f7f1
SumaVarshitha <sumavarshitha.kamatam997@gmail.com>
2020-07-27 13:32:37 +0530

    Update dockerbuild.groovy

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,5 +1,5 @@
-import groovy.json.*
-import groovy.json.JsonSlurper
+//import groovy.json.*
+//import groovy.json.JsonSlurper
 def call(dockerimage,mavenBuild) 
 {
         echo dockerimage
@@ -7,11 +7,11 @@ def call(dockerimage,mavenBuild)
         def dimage = dockerimage
         def mBuild = mavenBuild
         docker.image("${dockerimage}").inside(){
-          sh "${mBuild} -o result.json"
+          sh "${mBuild}"
         }
-         def jsonSlurper = new JsonSlurper()
+        /* def jsonSlurper = new JsonSlurper()
  def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/result.json"),"UTF-8"))
 def resultJson = jsonSlurper.parse(reader)
-        echo resultJson
+        echo resultJson*/
 }
             
