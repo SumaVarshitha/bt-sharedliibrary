64ecbbd823de813ff69d473f433f340ccf843b8e
SumaVarshitha <sumavarshitha.kamatam997@gmail.com>
2020-07-27 17:43:15 +0530

    Update dockerbuild.groovy

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -17,7 +17,7 @@ def call(dockerimage,mavenBuild)
               
         def buildlog = readFile "${WORKSPACE}/output.txt"
      // echo buildlog
-        if (buildlog.contains(Dependency-reduced)) {
+        if (buildlog.contains("Dependency-reduced")) {
     println "${job.name}: ${build.id}"
   }
        /* if(grep -w "Dependency-reduced" ${WORKSPACE}/output.txt)
